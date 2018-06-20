30ccc0060f568f88ab92bb303e94b8f39cef51e0
cssmagic <cssmagic.cn@gmail.com>
2018-06-21 00:14:20 +0800

    Add building script.

---
+++ webpack.mix.js
@@ -0,0 +1,54 @@
+const mix = require('laravel-mix')
+
+const envConfig  = require('dotenv').config()
+
+//设置全局参数
+const program = {
+    MIX_PROXY : envConfig.parsed.MIX_PROXY,	// BrowserSync 所用的代理
+    production : process.env.NODE_ENV === 'production',	// 是否生产环境
+}
+
+mix.disableNotifications()
+mix.options({
+	uglify: {
+		uglifyOptions: {
+			ie8: true,
+		},
+	},
+	autoprefixer: {
+		options: {
+			browsers: [
+				'Android >= 4.4',
+				'iOS >= 10',
+				'Chrome >= 35',
+				'Firefox >= 56',
+				'Safari >= 11',
+				'Explorer >= 11',
+			],
+		},
+	},
+	processCssUrls: false,
+})
+
+if (program.MIX_PROXY) {
+	mix.browserSync({
+		proxy: program.MIX_PROXY,
+		ui: false,
+		port: 3098,
+		files: [
+			'./demo/*.html',
+			'./demo/*.css',
+			'./demo/*.js',
+			'./dist/**/*.js',
+		],
+		// notify: false,
+		browser: [],
+	})
+}
+
+// tasks - js
+mix.js('./src/index.js', './dist/nasa.js')
+
+// task - css
+// mix.stylus('./demo/index.styl', './demo/')
+
