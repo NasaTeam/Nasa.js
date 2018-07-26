27806e61138d1582e8d37b4636c86a9d24260293
cssmagic <cssmagic.cn@gmail.com>
2018-07-26 17:24:24 +0800

    Delay call of `init()`.

--- src/util/init.js
+++ src/util/init.js
@@ -2,8 +2,10 @@ import * as user from '../user/index'
 import * as _addr from '../util/addr'
 
 export function init() {
-	user.getAddr()
-		.then((addr) => {
-			_addr.setAvailableAddr(addr)
-		})
+	document.addEventListener('DOMContentLoaded', () => {
+		user.getAddr()
+			.then((addr) => {
+				_addr.setAvailableAddr(addr)
+			})
+	}, false)
 }
