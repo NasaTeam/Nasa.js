385b3f0e1bec64bda8cdbb01b03151d97cf031bc
cssmagic <cssmagic.cn@gmail.com>
2018-07-31 23:47:24 +0800

    Minor updates.

--- src/_wrapper/dist-global.js
+++ src/_wrapper/dist-global.js
@@ -1,7 +1,7 @@
 /*! Nasa.js | LGPL-3.0 | https://github.com/cssmagic/Nasa.js */
 void function (window, document, undefined) {
 
-	// check conflict, and avoid repeated loading
+	// check conflict, and avoid repeated executing
 	if (window.Nasa) return
 
 	// provide a namespace for Nasa to mount
